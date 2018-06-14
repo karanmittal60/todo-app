a7edc48c2bb82908afc036e1d5728f0390f1894f
karanmittal60 <mittal.karan60@gmail.com>
2018-06-15 00:42:17 +0530

    first commit 1

---
+++ src/components/tasklist/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class TaskList extends React.Component{
+    render(){
+        return(
+            <table>
+                <thead>
+
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        );
+    }
+}
